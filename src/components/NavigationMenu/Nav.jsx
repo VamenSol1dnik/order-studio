b20bebe0fbb7185acb3fae03a04d65facdf4741e
VamenSol1dnik <valeravas03@gmail.com>
2024-08-14 14:18:33 +0300

    designing

---
+++ src/components/NavigationMenu/Nav.jsx
@@ -0,0 +1,17 @@
+function NavMenu() {
+    return(
+        <div>
+            <aside>
+                <nav>
+                    <ul>
+                        <li>Приход</li>
+                        <li>Группы</li>
+                        <li>продукты</li>
+                        <li>Пользователи</li>
+                        <li>Настройки</li>
+                    </ul>
+                </nav>
+            </aside>
+        </div>
+    )
+}
